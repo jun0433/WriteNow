6fb85554ffb04bed6fd6c0cd4d4dfe56b5a846d8
JunHwi <jubn0433@naver.com>
2023-05-01 16:27:45 +0900

    vector 이미지 추가 main color 추가 bottom nav 진행중

---
+++ app/src/main/java/com/example/writenow/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.writenow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNav);
+    }
+}
