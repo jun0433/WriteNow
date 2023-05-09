ecc50b1c1129f021a7d99b91cdb2d46b75102c13
JunHwi <jubn0433@naver.com>
2023-05-09 23:11:54 +0900

    bottom_nav 연결중

--- app/src/main/java/com/example/writenow/MainActivity.java
+++ app/src/main/java/com/example/writenow/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.writenow;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -16,5 +19,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         bottomNavigationView = findViewById(R.id.bottomNav);
+
+        getSupportFragmentManager().beginTransaction().add(R.id.main_frame, new HomeFragment()).commit();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    //item을 클릭시 id값을 가져와 FrameLayout에 fragment.xml띄우기
+                    case R.id.nav_home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new HomeFragment()).commit();
+                        break;
+                    case R.id.nav_write:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new WriteFragment()).commit();
+                        break;
+                    case R.id.nav_star:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new StarFragment()).commit();
+                        break;
+                    case R.id.nav_set:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new SetFragment()).commit();
+                        break;
+                }
+                return true;
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/writenow/SplashActivity.java
+++ app/src/main/java/com/example/writenow/SplashActivity.java
@@ -22,7 +22,7 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        },2000);
+        },3000);
     }
 
     @Override
