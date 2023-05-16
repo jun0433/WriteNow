8d14b78e898dcec811f721574df0f78d81ecd41a
JunHwi <jubn0433@naver.com>
2023-05-16 22:10:23 +0900

    bottom nav 연결 완료 버튼 구현 중 write 문항 작성중 fragement 연결

--- app/src/main/java/com/example/writenow/MainActivity.java
+++ app/src/main/java/com/example/writenow/MainActivity.java
@@ -2,48 +2,72 @@ package com.example.writenow;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
+
 
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.example.writenow.Home.HomeFragment;
+import com.example.writenow.Set.SetFragment;
+import com.example.writenow.Star.StarFragment;
+import com.example.writenow.Write.WriteFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static BottomNavigationView bottomNavigationView;
+
+    HomeFragment homeFragment;
+    WriteFragment writeFragment;
+    StarFragment starFragment;
+    SetFragment setFragment;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bottomNavigationView = findViewById(R.id.bottomNav);
+        init();
 
-        getSupportFragmentManager().beginTransaction().add(R.id.main_frame, new HomeFragment()).commit();
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
 
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()) {
-                    //item을 클릭시 id값을 가져와 FrameLayout에 fragment.xml띄우기
-                    case R.id.nav_home:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new HomeFragment()).commit();
-                        break;
-                    case R.id.nav_write:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new WriteFragment()).commit();
-                        break;
-                    case R.id.nav_star:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new StarFragment()).commit();
-                        break;
-                    case R.id.nav_set:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new SetFragment()).commit();
-                        break;
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.nav_home){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new HomeFragment()).commit();
+                    return true;
+                }
+                else if (id == R.id.nav_write){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new WriteFragment()).commit();
+                    return true;
+                }
+                else if (id == R.id.nav_star){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new StarFragment()).commit();
+                    return true;
+                }
+                else if (id == R.id.nav_set){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new SetFragment()).commit();
+                    return true;
                 }
                 return true;
             }
         });
+
+
+    }
+    private void init(){
+        //fragment 객체 생성
+        homeFragment = new HomeFragment();
+        writeFragment = new WriteFragment();
+        starFragment = new StarFragment();
+        setFragment = new SetFragment();
+
+        //main.xml상의 bottomNavigationView 연결
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+
+        //제일 처음 띄어줄 뷰 세팅
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame,homeFragment).commitAllowingStateLoss();
 
     }
 }
