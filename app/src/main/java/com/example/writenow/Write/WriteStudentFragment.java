84bab21da72141fd89230690c370c8a3ecb54734
barcoding11111 <cadisdi77@gmail.com>
2023-05-17 11:09:18 +0900

    write 문항 작성 내용 받아주기

--- app/src/main/java/com/example/writenow/Write/WriteStudentFragment.java
+++ app/src/main/java/com/example/writenow/Write/WriteStudentFragment.java
@@ -7,16 +7,34 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import com.example.writenow.R;
 
 
 public class WriteStudentFragment extends Fragment {
+    private EditText userInputEditText;
+    private Button submitButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_write_student, container, false);
+        View view = inflater.inflate(R.layout.fragment_write_student,container,false);
+        userInputEditText = view.findViewById(R.id.editText);
+        submitButton = view.findViewById(R.id.appCompatButton);
+
+        submitButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String userInput = userInputEditText.getText().toString();
+                handleUerInput(userInput);
+            }
+        });
+        return view;
+    }
+    private void handleUerInput(String userInput){
+        Toast.makeText(getActivity(),"사용자 입력: "+ userInput,Toast.LENGTH_SHORT).show();
     }
 }
